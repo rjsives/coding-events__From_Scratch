d26f36d21b46a32dffb86408102f2faa291166ff
Rich Ives <rjsives89@gmail.com>
2022-10-05 23:49:50 -0500

    delete event functionality added

--- src/main/java/org/lauchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/lauchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,6 @@
 package org.lauchcode.codingevents.controllers;
 
+import data.EventData;
 import org.lauchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,15 +17,10 @@ import java.util.List;
 @RequestMapping("events")
 public class EventController {
 
-    private static List<Event> events = new ArrayList<>();
+
     @GetMapping
     public String displayAllEvents(Model model){
-
-//        events.put("Greentree Festival", "Kirkwood at its folksiest");
-//        events.put("Forest Park Balloon Race", "Feel the glow!");
-//        events.put("New Baden fest", "Revitalizing the north.");
-
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
 
@@ -37,7 +33,24 @@ public class EventController {
     @PostMapping("create")
     public String createEvent(@RequestParam String eventName,
                               @RequestParam String eventDescription) {
-        events.add(new Event(eventName, eventDescription));
+       EventData.add(new Event(eventName, eventDescription));
+        return "redirect:";
+    }
+
+    @GetMapping("delete")
+        public String renderDeleteEventForm(Model model){
+            model.addAttribute("title", "Delete Event");
+            model.addAttribute("events", EventData.getAll());
+            return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String deleteEvent(@RequestParam(required = false) int[] eventIds){
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
         return "redirect:";
     }
 }
